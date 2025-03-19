fd222898c3adb39c408fac1adc6135e62efe8605
developerTamanna <astamanna57@gmail.com>
2025-03-19 21:15:06 +0600

    set get object

--- script/scriot.js
+++ script/scriot.js
@@ -4,8 +4,26 @@
 
 // localStorage.getItem("my age")
 
-const handleAddToStorage=()=>{
-    // console.log('hiiii')
-    const name = 'tamanna'
-    localStorage.setItem("name", name)
+// const handleAddToStorage=()=>{
+//     // console.log('hiiii')
+//     const name = 'tamanna'
+//     localStorage.setItem("name", name)
+// }
+
+
+
+// **    object
+
+
+const handleAddToStorage =()=>{
+    const person = {
+        name:'rumpa',
+        age: 43,
+        friend:["lima ,ruba"]
+    }
+    // localStorage.setItem('details', person)
+    const covertedString = JSON.stringify(person);
+    const persObject = JSON.parse(covertedString)
+    console.log(persObject.friend)
+    localStorage.setItem("details", covertedString)
 }
