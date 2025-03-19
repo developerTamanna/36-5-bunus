b9f271d2357fbace1bb3c8b97916a3be6c67525b
developerTamanna <astamanna57@gmail.com>
2025-03-19 21:04:22 +0600

    set item

--- script/scriot.js
+++ script/scriot.js
@@ -3,3 +3,9 @@
 // localStorage.setItem("my age", age)
 
 // localStorage.getItem("my age")
+
+const handleAddToStorage=()=>{
+    // console.log('hiiii')
+    const name = 'tamanna'
+    localStorage.setItem("name", name)
+}
